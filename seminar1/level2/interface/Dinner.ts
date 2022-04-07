8c93e4cd672bc493dd189cd673ba800b8219f9f6
kmebin <blowhuiee@gmail.com>
2022-04-07 17:55:40 +0900

    [ADD] 발전 과제 (Level 2)

---
+++ seminar1/level2/interface/Dinner.ts
@@ -0,0 +1,8 @@
+import { Member } from './Member';
+
+export interface Dinner {
+  member: Member[];
+
+  shuffle(array: Member[]): Member[];
+  organize(array: Member[]): void;
+}
---
+++ seminar1/level2/random.ts
@@ -0,0 +1,42 @@
+import { Dinner } from './interface/Dinner';
+
+const dinner: Dinner = {
+  member: [
+      {
+          name: '채정아',
+          group: 'ob'
+      },
+      {
+          name: '김동재',
+          group: 'yb'
+      },
+      {
+          name: '강민재',
+          group: 'yb'
+      },
+      {
+          name: '김루희',
+          group: 'ob'
+      },
+      {
+          name: '박진수',
+          group: 'ob'
+      }
+  ],
+  shuffle(array) {
+      array.sort(() => Math.random() - 0.5);
+
+      return array;
+  },
+  organize(array) {
+      this.shuffle(array);
+
+      const ob = array.find(o => o.group === 'ob');
+      const yb = array.find(o => o.group === 'yb');
+      const dinnerMember = [ob?.name, yb?.name]
+
+      console.log(`오늘의 저녁 식사 멤버는 ${dinnerMember[0]}, ${dinnerMember[1]}`);
+  }
+};
+
+dinner.organize(dinner.member);
