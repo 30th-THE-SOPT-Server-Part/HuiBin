ff76421ca7e58aeb99c0965b39b8b6b9291b7285
kmebin <blowhuiee@gmail.com>
2022-05-29 18:17:32 +0900

    [UPDATE] TimeStamp 옵션 추가

--- seminar7/src/models/Movie.ts
+++ seminar7/src/models/Movie.ts
@@ -1,24 +1,27 @@
 import mongoose from 'mongoose';
 import { MovieInfo } from '../interfaces/movie/movieInfo';
 
-const MovieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const MovieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    director: {
+      type: String,
+      required: true,
+    },
+    openingDate: {
+      type: Date,
+    },
+    thumbnail: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
   },
-  director: {
-    type: String,
-    required: true,
-  },
-  openingDate: {
-    type: Date,
-  },
-  thumbnail: {
-    type: String,
-  },
-  content: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
 export default mongoose.model<MovieInfo & mongoose.Document>('Movie', MovieSchema);
--- seminar7/src/models/Review.ts
+++ seminar7/src/models/Review.ts
@@ -1,24 +1,27 @@
 import mongoose from 'mongoose';
 import { ReviewInfo } from '../interfaces/review/reviewInfo';
 
-const ReviewSchema = new mongoose.Schema({
-  writer: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'User',
+const ReviewSchema = new mongoose.Schema(
+  {
+    writer: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    movie: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Movie',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+    },
   },
-  movie: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-    ref: 'Movie',
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
 export default mongoose.model<ReviewInfo & mongoose.Document>('Review', ReviewSchema);
